a925a3315042b937f27760e915680bbf5e49a46f
Jacob Logas <jakeman893@ufl.edu>
2018-04-05 17:59:15 -0400

    Using log function

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -114,7 +114,7 @@ def Shows(url, title):
     # Add shows by parsing the site
     element = HTML.ElementFromURL(url)
 
-    print(element)
+    Log(element)
 
     for item in element.xpath("//*[@class = 'square-blocks']//a"):
         show = {}

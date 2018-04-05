4606e8fa4bd07035959d073e7261273d22ff6607
Jacob Logas <jakeman893@ufl.edu>
2018-04-05 17:25:44 -0400

    Some updates for images

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -12,47 +12,47 @@ CHANNELS = [
     {
         'title': 'Rooster Teeth',
         'url': 'rooster-teeth',
-        'image': 'https://pbs.twimg.com/profile_images/699276126644936704/rCkvY0SS_400x400.jpg'
+        'image': 'https://www.austinchronicle.com/binary/9a84/RESIZED.roosterteeth.jpg'
     },
     {
         'title': 'Achievement Hunter',
         'url': 'achievement-hunter',
-        'image': 'https://pbs.twimg.com/profile_images/1834741417/283025_10150250994985698_268895495697_7664630_357027_n_400x400.jpg'
+        'image': 'https://vignette.wikia.nocookie.net/roosterteeth/images/a/a2/Achievement_Hunter_icon.png'
     },
     {
         'title': 'Funhaus',
         'url': 'funhaus',
-        'image': 'https://pbs.twimg.com/profile_images/563456577076596737/kTHggklU_400x400.png'
+        'image': 'https://ia.media-imdb.com/images/M/MV5BZDFlMjI0NGEtYTFhNS00NThmLWIyM2QtYzAwNGEwMzc3ZWZhXkEyXkFqcGdeQXVyNDM3MDIyOTk@._V1_.jpg'
     },
     {
         'title': 'ScrewAttack',
         'url': 'screwattack',
-        'image': 'https://pbs.twimg.com/profile_images/735516290773704704/gJZmqxDZ_400x400.jpg'
+        'image': 'https://vignette.wikia.nocookie.net/roosterteeth/images/7/7c/ScrewAttack_logo.png'
     },
     {
         'title': 'Game Attack',
         'url': 'game-attack',
-        'image': 'https://pbs.twimg.com/profile_images/783404372323475456/A3XK5iD7_400x400.jpg'
+        'image': 'https://vignette.wikia.nocookie.net/roosterteeth/images/d/da/Game_Attack_logo.png'
     },
     {
         'title': 'The Know',
         'url': 'the-know',
-        'image': 'https://pbs.twimg.com/profile_images/639837776934891520/WA-rAvdP_400x400.png'
+        'image': 'https://yt3.ggpht.com/-0lKHa9Wjo54/AAAAAAAAAAI/AAAAAAAAAAA/fVDFDWwii7A/s900-c-k-no/photo.jpg'
     },
     {
         'title': 'Cow Chop',
         'url': 'cow-chop',
-        'image': 'https://pbs.twimg.com/profile_images/671530901734473728/CfowRP9t_400x400.png'
+        'image': 'https://vignette.wikia.nocookie.net/roosterteeth/images/8/8b/Cow_Chop_logo.png'
     },
     {
         'title': 'Sugar Pine 7',
         'url': 'sugar-pine-7',
-        'image': ''
+        'image': 'https://ih1.redbubble.net/image.439643676.5794/flat,550x550,075,f.u1.jpg'
     },
     {
         'title': 'JT Music',
         'url': 'jt-music',
-        'image': ''
+        'image': 'https://s3.amazonaws.com/cdn.roosterteeth.com/uploads/images/0ab6e554-8004-4ef9-96db-1098c4062921/md/2528776-1508766723636-Social_Logo_Red.png'
     }
 ]
 

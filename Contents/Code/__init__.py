a8d391aad020d71f8ce2144656371d778c3f980c
Jacob Logas <jakeman893@ufl.edu>
2018-04-05 20:40:48 -0400

    Trying to log stillllllll

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -114,10 +114,10 @@ def Shows(url, title):
     # Add shows by parsing the site
     element = HTML.ElementFromURL(url)
 
-    Log.Info("%s" %url, True)
-    Log.Info("%s" %SERIES_URL, True)
+    Log.Info(str(url), True)
+    Log.Info(str(SERIES_URL), True)
 
-    Log.Info("%s" %element, True)
+    Log.Info(str(element), True)
 
     for item in element.xpath('//div[contains (@class, "card-image-wrapper")]'):
         show = {}

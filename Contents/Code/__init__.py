d517b596f0bfa293aba1d4f8b1bb4aae0a55c747
Jacob Logas <jakeman893@ufl.edu>
2018-04-07 10:45:34 -0400

    Add necessary submodules

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -1,60 +1,6 @@
+from config import *
+
 Login = SharedCodeService.roosterteeth.Login
-
-TITLE = 'Rooster Teeth'
-ART   = 'art-default.jpg'
-ICON  = 'icon-default.png'
-BASE_URL = 'http://roosterteeth.com'
-HTTP_USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_5) AppleWebKit/536.30.1 (KHTML, like Gecko) Version/6.0.5 Safari/536.30.1'
-SERIES_URL = "https://roosterteeth.com/series?"
-
-
-CHANNELS = [
-    {
-        'title': 'Rooster Teeth',
-        'url': 'rooster-teeth',
-        'image': 'https://www.austinchronicle.com/binary/9a84/RESIZED.roosterteeth.jpg'
-    },
-    {
-        'title': 'Achievement Hunter',
-        'url': 'achievement-hunter',
-        'image': 'https://vignette.wikia.nocookie.net/roosterteeth/images/a/a2/Achievement_Hunter_icon.png'
-    },
-    {
-        'title': 'Funhaus',
-        'url': 'funhaus',
-        'image': 'https://ia.media-imdb.com/images/M/MV5BZDFlMjI0NGEtYTFhNS00NThmLWIyM2QtYzAwNGEwMzc3ZWZhXkEyXkFqcGdeQXVyNDM3MDIyOTk@._V1_.jpg'
-    },
-    {
-        'title': 'ScrewAttack',
-        'url': 'screwattack',
-        'image': 'https://vignette.wikia.nocookie.net/roosterteeth/images/7/7c/ScrewAttack_logo.png'
-    },
-    {
-        'title': 'Game Attack',
-        'url': 'game-attack',
-        'image': 'https://vignette.wikia.nocookie.net/roosterteeth/images/d/da/Game_Attack_logo.png'
-    },
-    {
-        'title': 'The Know',
-        'url': 'the-know',
-        'image': 'https://yt3.ggpht.com/-0lKHa9Wjo54/AAAAAAAAAAI/AAAAAAAAAAA/fVDFDWwii7A/s900-c-k-no/photo.jpg'
-    },
-    {
-        'title': 'Cow Chop',
-        'url': 'cow-chop',
-        'image': 'https://vignette.wikia.nocookie.net/roosterteeth/images/8/8b/Cow_Chop_logo.png'
-    },
-    {
-        'title': 'Sugar Pine 7',
-        'url': 'sugar-pine-7',
-        'image': 'https://ih1.redbubble.net/image.439643676.5794/flat,550x550,075,f.u1.jpg'
-    },
-    {
-        'title': 'JT Music',
-        'url': 'jt-music',
-        'image': 'https://s3.amazonaws.com/cdn.roosterteeth.com/uploads/images/0ab6e554-8004-4ef9-96db-1098c4062921/md/2528776-1508766723636-Social_Logo_Red.png'
-    }
-]
 
 ##########################################################################################
 def Start():
@@ -118,6 +64,8 @@ def Shows(url, title):
     # Log.Info("The series url is %s" % SERIES_URL, True)
 
     # Log.Info(str(element), True)
+
+    Log.Info("Hello", True)
 
     for item in element.xpath('//div[contains (@class, "card-image-wrapper")]'):
         show = {}

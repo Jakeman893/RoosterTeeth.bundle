0fa429a41c25dd24378d1b9e83c79daf129217b3
Jacob Logas <jakeman893@ufl.edu>
2018-04-08 20:20:07 -0400

    Trying some url magic

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -46,7 +46,7 @@ def MainMenu():
     return oc
 
 ##########################################################################################
-@route('/video/roosterteeth/Shows')
+@route('/video/roosterteeth/<channel>/shows')
 def Shows(channel):
     oc = ObjectContainer(title2=channel)
     
@@ -153,81 +153,9 @@ def RecentEpisodes(channel):
                 title = episode.title,
                 thumb = episode.thumbnail,
                 summary = episode.description,
-                url = episode.video.get_quality(),
+                url = episode.canonical_url,
                 season = episode.season.number,
                 duration = episode.length
             )
         )
-    return oc
-
-##########################################################################################
-@route("/video/roosterteeth/Items", recent = bool)
-def Items(title, url, thumb, xpath_string, art, id=None, recent=False):
-
-    oc = ObjectContainer(title2=title)
-
-    element = HTML.ElementFromURL(url)
-
-    episodes = []
-    for item in element.xpath(xpath_string):
-
-        if id:
-            season_id = item.xpath(".//label/@for")[0]
-
-            if id != season_id:
-                continue
-
-            try:
-                season = int(id.split(" ")[1])
-            except:
-                season = None
-
-        else:
-            season = None
-
-        for episode in item.xpath(".//*[@class='grid-blocks']//li"):
-
-            url = episode.xpath("./a/@href")[0]
-            title = episode.xpath(".//*[@class='name']/text()")[0]
-            thumb = episode.xpath(".//img/@src")[0]
-
-            if thumb.startswith("//"):
-                thumb = 'http:' + thumb
-
-            try:
-                index = int(title.split(" ")[1])
-            except:
-                index = None
-
-            try:
-                duration_string = episode.xpath(".//*[@class='timestamp']/text()")[0].strip()
-                duration = ((int(duration_string.split(":")[0])*60) + int(duration_string.split(":")[1])) * 1000
-            except:
-                duration = None
-
-            episodes.append(
-                EpisodeObject(
-                    url = url,
-                    title = title,
-                    thumb = thumb,
-                    season = season,
-                    index = index,
-                    duration = duration,
-                    art = art
-                )
-            )
-
-    if Prefs['sort'] == 'Latest First' or recent:
-        for episode in episodes:
-            oc.add(episode)   
-    else:
-        for episode in reversed(episodes):
-            oc.add(episode)
-
-    if len(oc) < 1:
-        return ObjectContainer(
-            header = "Sorry",
-            message = "Couldn't find any videos for this show"
-        )
-
     return oc

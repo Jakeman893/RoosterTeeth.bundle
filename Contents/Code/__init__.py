f6c7b560b636afe5cfddc89ddbffbf723b932a70
Jacob Logas <jakeman893@ufl.edu>
2018-04-10 22:14:12 -0400

    Try HTTPMP4Streaming

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -140,7 +140,7 @@ def RecentEpisodes(channel):
                         optimized_for_streaming = True,
                         parts = [
                             PartObject(
-                                key = HTTPMP4Video(episode.video.get_quality())
+                                key = HTTPMP4Streaming(episode.video.get_quality())
                             )
                         ]
                     )

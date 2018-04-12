b8e82558868aca76807bcefc5efb199f23623c9d
Jacob Logas <jakeman893@ufl.edu>
2018-04-12 14:33:47 -0400

    Try to play just first ts file

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -203,16 +203,18 @@ def PlayOfflineStream(url, **kwargs):
 def PlayVideo(url, **kwargs):
     # parts = []
     Log.Info('Getting video files for %s' % (url))
-    # m3u8_obj = m3u8.load(m3u8_url)
+    m3u8_obj = m3u8.load(m3u8_url)
+
+    return m3u8_obj.segments[0].absolute_uri
+
+    # try:
+    #     res = requests.get(url)
+    # except requests.exceptions.SSLError:
+    #     res = requests.get(url, verify=False)
+    #     Log.Info("Warning: SSL Certificate Error")
+    #     pass
     
-    try:
-        res = requests.get(url)
-    except requests.exceptions.SSLError:
-        res = requests.get(url, verify=False)
-        Log.Info("Warning: SSL Certificate Error")
-        pass
-    
-    return res.text
+    # return res.text
     # m3u8_obj = m3u8.loads(url.text)
 
     # for seg in m3u8_obj.segments:
